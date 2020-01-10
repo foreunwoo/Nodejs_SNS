66fc19c740549d2e721b45e097ab361539740c89
suuum12 <57670228+suuum12@users.noreply.github.com>
2020-01-10 23:43:52 +0900

    db에 접근

---
+++ models/index.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database, config.username, config.password, config,
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize; 
+
+db.User = require('./user')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);
+
+db.User.hasMany(db.Post);
+db.Post.belongsTo(db.User);
+//일대다 관계
+
+db.Post.belongsToMany(db.Hashtag,{through:'PostHashtag',});
+db.Hashtag.belongsToMany(db.Post,{through: 'PostHashtag'});
+//다대다 관계
+
+db.User.belongsToMany(db.User,{ //일반인
+  through:'Follow',
+  as:'Followers', 
+  foreignKey:'followingId'
+});
+
+db.User.belongsToMany(db.User,{ //유명인
+  through:'Follow', 
+  as:'Followings', 
+  foreignKey:'followerId'
+});
+//유저와 유저관의 관계
+
+db.User.belongsToMany(db.Post,{through:'Like'});
+db.Post.belongsToMany(db.User,{through:'Like', as:'Liker'});
+//좋아요
+
+
+
+module.exports = db;
